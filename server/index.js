ad0da737dbd6738bef7935a5111ef03157303406
Emiliar2002 <earancio616@gmail.com>
2022-07-05 19:50:16 -0300

    Creacion de usuario en la db por POST request

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+// Seteado en force: true para que sea facil debuggear.
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
