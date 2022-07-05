940eabac21d82b1a502d50b76faf2b10a0979a86
tinchorlando <martin._orlando@hotmail.com.ar>
2022-07-05 16:23:00 -0300

    Add basic components: AdminHub/Cart/CreateUser/Detail/Landing/Nav/Profile

---
+++ client/src/components/CreateUser/index.jsx
@@ -0,0 +1,18 @@
+
+
+export default function NewUser (){
+    return(
+        <div>
+            <header>
+                <h1>Welcome user</h1>
+            </header>
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+            </form>
+        </div>
+    )
+}
