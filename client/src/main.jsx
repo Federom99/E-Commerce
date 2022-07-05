958a1a66071dfd4cd37c523dd03bc01d0750a5ba
tinchorlando <martin._orlando@hotmail.com.ar>
2022-07-05 16:55:29 -0300

    Add Routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { lightTheme } from './styles/themes'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>      
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 )
