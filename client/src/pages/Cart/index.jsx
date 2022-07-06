3fbd3f9724142a03de4072ee57d9cf4697038b1e
Davigetz <Davidstangetz13@gmail.com>
2022-07-05 22:30:02 -0500

    client-pages-LeftFilter-search-navbarDesign

---
+++ client/src/pages/Cart/index.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Cart (){
+    return(
+        <div>
+            <h1>
+                Shopping cart
+            </h1>
+        </div>
+    )
+}
---
+++ client/src/pages/Profile/index.jsx
@@ -0,0 +1,9 @@
+import Footer from "../../components/Footer";
+
+export default function User() {
+  return (
+    <div style={{ paddingTop: "90px" }}>
+      <h1>I'm an user and IM ALIVE</h1>
+    </div>
+  );
+}
