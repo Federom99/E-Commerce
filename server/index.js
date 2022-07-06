e7015ff0efdfd8c654edac4db9b6f0fc9e96b028
Emiliar2002 <earancio616@gmail.com>
2022-07-06 01:45:23 -0300

    Query product search (GET /products?name=)

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { Producto } = require("./src/db.js");
 const fs = require("fs");
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log("%s listening at 3001");
     const productosJSON = JSON.parse(
--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,20 +1,35 @@
 const { Router } = require("express");
 const { Producto } = require("../db.js");
+const {Op} = require('sequelize')
 
 const router = Router();
 
 router.get('/', async(req, res) => {
 
-    const productsSearch = await Producto.findAll();
+    const {name} = req.query
 
-    console.log(productsSearch)
+    //Traigo todos los productos de la db
+    let productsSearch
 
-    let productos = productsSearch.map(p => {
-        console.log(p)
+    //Si no me pasan name por query, traigo todos los productos.
+    if(!name) productsSearch = await Producto.findAll();
+    //Si me lo pasan, traigo los productos que tengan un nombre parecido.
+    else productsSearch = await Producto.findAll({
+        where:{
+            nombre:{
+                [Op.iLike]: `%${name}%`,
+            }
+        }
+    });
+
+    //Los mapeo para que no se vea la informacion innecesaria de sequelize
+    const productos = productsSearch.map(p => {
         return p.dataValues
     })
 
-    res.send(productos)
+
+    //Envio los productos como respuesta
+    res.status(200).send(productos)
 
 
 })
