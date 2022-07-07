94eebb24433af8de0da02d3a7bf8f4b40666156e
tinchorlando <martin._orlando@hotmail.com.ar>
2022-07-07 01:52:59 -0300

    Agrego popUp: proyecto de popUp al añadir productos al carrito, previo al ingreso del  mismo

---
+++ client/src/components/PopUp/styles.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+export const MainDiv = styled.div`
+    background-color:yellow;
+    width:900px;
+    heigth:auto;
+    display:flex;
+    flex-direction:column;
+    align-items: center;
+    position:relative;
+`
+export const Div = styled.div`
+
+`;
+
+export const Header = styled.header`
+
+`;
+export const Close = styled.button`
+    position:absolute;
+    top:0px;
+    right:0px;
+`
+
+export const Main = styled.main`
+    width: inherit;
+    display:flex;
+    align-items:center;
+    justify-content:space-around
+`;
+
+export const Detail = styled.section`
+
+`;
+
+export const List = styled.ul`
+    list-style: none;
+`;
+export const Img = styled.img`
+    max-width:260px;
+    height:auto;
+    margin:30px 0 30px
+`;
+
+export const Li = styled.li`
+
+`;
+
+export const Options = styled.section`
+
+`;
+
+export const Button = styled.button`
+
+`;
+export const IncDiv = styled.div`
+    display:flex;
+`
