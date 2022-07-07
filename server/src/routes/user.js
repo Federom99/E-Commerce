e9c926a6d1c4e0d043e13f25796cf26dbaf278de
Emiliano Arancio <93626345+Emiliar2002@users.noreply.github.com>
2022-07-07 15:42:32 -0300

    GET /user/:id

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -4,9 +4,23 @@ const {
   register,
 } = require("../controllers/user.controller.js");
 
+const {Usuario} = require('../db')
+
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", authentication);
+
+router.get('/:id', async (req, res) => {
+  let {id} = req.params
+  
+  //Parseo el id
+
+  const foundUser = await Usuario.findByPk(id)
+
+  if(!foundUser) return res.status(404).send({Error:'Usuario no encontrado.'})
+  else return res.send(foundUser)
+
+})
 
 module.exports = router;
