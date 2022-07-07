9a8036e635711ac331661c81ac8cd8f1ef2f1774
Jose Osorio <joseinvictus273@hotmail.com>
2022-07-07 13:49:16 -0500

    Feat: filling the product details component with the api data

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,9 +2,16 @@ export const getProducts = () => {
     return async function(dispatch){
         const response = await fetch("http://localhost:3001/products")
         const data = await response.json()
-        console.log(data)
         if(data !== undefined){
             dispatch({type: 'GET_PRODUCTS', payload: data})
         }
+    }
+}
+
+export const getProduct = (productId) => {
+    return async function (dispatch){
+        const response = await fetch(`http://localhost:3001/product/${productId}`)
+        const data = await response.json()
+        dispatch({type: 'GET_PRODUCT', payload: data})
     }
 }
