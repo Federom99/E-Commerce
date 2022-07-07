f4596a830e9f8d9fee26c6d31c0dccb177c94a4b
rgzrgzm <mariomkt97@gmail.com>
2022-07-07 16:29:07 -0700

    register - login users front/back connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,9 @@ import AdminHub from "./pages/Admin";
 import CreateUser from "./pages/CreateUser";
 import ShoppingCart from "./pages/Cart";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import Footer from "./components/Footer";
-import Login from "./pages/login/Login";
 
 function App() {
   const location = useLocation();
@@ -22,8 +22,9 @@ function App() {
         <Route exact path="/" element={<MainContainer />} />
         <Route path="/detail/:productId" element={<ProductDetail />} />
         <Route path="/admin/" element={<AdminHub />} />
+        <Route path="/login/" element={<Login />} />
+        <Route path="/register" element={<CreateUser />} />
         <Route path="/newUser" element={<CreateUser />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/cart/" element={<ShoppingCart />} />
         <Route path="/profile/" element={<Profile />} />
       </Routes>
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const {
   authentication,
   register,
+  getUsers,
 } = require("../controllers/user.controller.js");
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", authentication);
+router.get("/", getUsers)
 
 module.exports = router;
