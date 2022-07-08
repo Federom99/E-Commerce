2739397f734993c2e036fa3b7f8a33d314fad85c
tinchorlando <martin._orlando@hotmail.com.ar>
2022-07-07 22:44:36 -0300

    componente contenedor de carrito

---
+++ client/src/containers/CartContainer/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    width: inherit;
+    align-items: center;
+`
+export const Li = styled.li`
+
+`
