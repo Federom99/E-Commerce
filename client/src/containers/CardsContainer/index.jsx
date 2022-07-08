302d4b542b3d9556a0faf3083b6e00431568bc74
tinchorlando <martin._orlando@hotmail.com.ar>
2022-07-07 22:46:10 -0300

    final mods to cart

--- client/src/containers/CardsContainer/index.jsx
+++ client/src/containers/CardsContainer/index.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
-
 import Card from "../../components/Card";
 import { Section } from "./styles";
 import { getProducts } from '../../redux/actions';
