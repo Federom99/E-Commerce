10b132dd3ac4826beb2dadc9e75c90397c93d068
AlanAcevedo12 <101600280+AlanAcevedo12@users.noreply.github.com>
2022-07-07 23:10:52 -0300

    Update products.js

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -6,10 +6,16 @@ const router = Router();
 
 router.get('/:page', async(req, res) => {
 
-    const {name} = req.query
+    /*const {name} = req.query
     let {page} = req.params
     page = parseInt(page)
-    const paginated = 25
+    const paginated = 25*/
+    
+    //provisional
+    const {name} = req.query
+    let page = 1;
+    page = parseInt(page)
+    const paginated = 250;
 
     //En caso de que me pidan una pagina menor o igual a 0 o que no sea un stream, mando un error.
     if(page <= 0 || !page) return res.status(400).send({Error: 'Las paginas deben ser números que empiezan desde el 1.'})
