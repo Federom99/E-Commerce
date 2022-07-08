32ad9dbc9d7ad9647397a870f651a3b9a830b9ee
rgzrgzm <mariomkt97@gmail.com>
2022-07-07 19:33:50 -0700

    fixing merge  register/login

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,85 +1,109 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import {
-    Div,
-    DivBtn,
-    Input,
-    List,
-    Google,
-    Gith,
-    InputDiv,
-    Section,
-    Form,
-    Blist
-} from "./styles"
-export default function Login (){
-    const [newUser,setNewUser]=useState({})
-    const [errors,setErrors]=useState({})
-    const navigate = useNavigate()
-    const errorHandler = (data)=>{
+  Div,
+  DivBtn,
+  Input,
+  List,
+  Google,
+  Gith,
+  InputDiv,
+  Section,
+  Form,
+  Blist,
+} from "./styles";
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alert, setAlert] = useState({});
+  const navigate = useNavigate();
 
-        if (!data.email) errors.email='Obligatory field';
-        if (data.email){
-            if (!/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/.test(data.email)) errors.email='Sumbit a valid email'
-        }
-        if (!data.password) errors.password='obligatory field'
-        if (data.password){
-            if (!/(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/.test(data.password)) errors.password='Password must have at least 1 upper case, 1 lower case, one number and 8 characters long'
-        }
+  useEffect(() => {
+    const userSession = localStorage.getItem("token");
+    if (userSession) {
+      navigate("/");
     }
-    const handleChange = (event)=>{
-        setNewUser({
-            ...newUser,
-            [event.target.name]:event.target.value,
-        })
-        setErrors(errorHandler({
-            ...newUser,
-            [event.target.name]:event.target.value
-        }))
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if ([email, password].includes("")) {
+      setAlert({ msg: "All fields are required", type: "error" });
+      return;
     }
-    const handleSubmit = (event)=>{
-        event.preventDefault();
-        if(!Object.keys(errors).length && Object.keys(newUser).length){
-            console.log(newUser)            
-        }
-        else {
-            alert('Check for errors')
-            console.log(errors)
-        }
+
+    try {
+      const { data } = await axios.post("http://localhost:3001/user/login", {
+        mail: email,
+        contraseña: password,
+      });
+
+      localStorage.setItem("token", data.token);
+      navigate("/");
+    } catch (error) {
+      setAlert({ msg: error.response.data.msg, type: "error" });
     }
-    const register = ()=>{
-        navigate("/register")
-    }
-    return(
-        <Div>
-            <Section>
-            <DivBtn>
-                <Google>
-                    Sign in with Google
-                </Google>
-                <Gith>
-                    Sign in with Github
-                </Gith>
-            </DivBtn>
-            <InputDiv>
-                <Form onSubmit={handleSubmit}>                 
-                    <List>
-                        <li>                        
-                            <Input placeholder="e-mail" type="text" name="email" onChange={handleChange}/>
-                        </li>
-                        <li>
-                            <Input placeholder="password" type="text" name="password" onChange={handleChange}/>
-                        </li>
-                    </List>
-                </Form>            
-                <Blist>
-                    <li><button onClick={register} > Sign up</button></li>
-                    <li><button type="submit">Log in</button></li>
-                </Blist>
-            </InputDiv>
-            </Section>
-            
-            
-        </Div>
-    )
+  };
+
+  const { msg } = alert;
+
+  return (
+    <Div>
+      <Section>
+        <DivBtn>
+          <Google>Sign in with Google</Google>
+          <Gith>Sign in with Github</Gith>
+        </DivBtn>
+        <InputDiv>
+          <Form onSubmit={handleSubmit}>
+            <List>
+              <li>
+                <Input
+                  placeholder="e-mail"
+                  type="text"
+                  name="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </li>
+              <li>
+                <Input
+                  placeholder="password"
+                  type="password"
+                  name="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </li>
+            </List>
+
+            <Blist>
+              <li>
+                <button onClick={() => navigate("/register")}> Sign up</button>
+              </li>
+              <li>
+                <button type="submit">Log in</button>
+              </li>
+            </Blist>
+          </Form>
+
+          {alert.msg && alert.type === "error" && (
+            <p
+              style={{
+                marginTop: 20,
+                fontSize: 13,
+                backgroundColor: "red",
+                color: "#fff",
+                textAlign: "center",
+                padding: 5,
+                borderRadius: 5,
+              }}
+            >
+              {msg}
+            </p>
+          )}
+        </InputDiv>
+      </Section>
+    </Div>
+  );
 }
