500ff99f0ca6bc808e37a68f9dbadbd23104b358
Federom99 <federo.15.99@gmail.com>
2022-07-10 12:55:27 -0300

    fix

--- server/src/routes/product-DB.js
+++ server/src/routes/product-DB.js
@@ -1,14 +1,17 @@
 const { Router } = require("express");
-const { Producto} = require("../db.js");
+const { Producto } = require("../db.js");
 
 const router = Router();
 
-
 // POST cargar productos en la base de datos
 router.post("/", async (req, res) => {
+  try {
     const producto = await Producto.bulkCreate(productosDB);
-  
-    res.status(200).send(producto);
-  });
 
-  module.exports = router;
+    res.status(200).send(producto);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
--- server/src/routes/user-DB.js
+++ server/src/routes/user-DB.js
@@ -3,12 +3,15 @@ const { Usuario } = require("../db.js");
 
 const router = Router();
 
-
 // POST cargar usuarios en la base de datos
 router.post("/", async (req, res) => {
+  try {
     const producto = await Usuario.bulkCreate(usuariosDB);
-  
-    res.status(200).send(producto);
-  });
 
-  module.exports = router;
+    res.status(200).send(producto);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
