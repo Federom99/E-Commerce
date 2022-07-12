c2bb2cd05635a6f757a4cec4647b8bc0ef4419de
Emiliano Arancio <93626345+Emiliar2002@users.noreply.github.com>
2022-07-12 20:48:37 -0300

    removed stock from product model and handling negative numbers

--- server/src/routes/stock.js
+++ server/src/routes/stock.js
@@ -1,36 +1,42 @@
 const { Router } = require("express");
-const {Producto_talle, Talle} = require("../db");
+const { Producto_talle, Talle } = require("../db");
 const router = Router();
 
-router.put('/:productId', async(req, res) => {
+router.put("/:productId", async (req, res) => {
+  const { productId } = req.params;
 
-    const {productId} = req.params
+  const { talle, stock } = req.body;
 
-    const {talle, stock} = req.body
+  try {
+    //Busco el talle en la db por nombre.
+    const talleInDb = await Talle.findOne({ where: { talle: talle } });
+    const talleId = talleInDb.dataValues.id;
 
-    try{
-        //Busco el talle en la db por nombre
-        const talleInDb = await Talle.findOne({where: {talle: talle}})
-        const talleId = talleInDb.dataValues.id
+    //Actualizo el stock de la prenda de ese talle segun lo que me pasen por el body.
+    await Producto_talle.update(
+      { stock: stock },
+      {
+        where: {
+          productoId: productId,
+          talleId: talleId,
+        },
+      }
+    );
 
-        //Actualizo el stock de la prenda de ese talle segun lo que me pasen por el body
-        await Producto_talle.update({stock: stock}, {
-            where: {
-                productoId: productId,
-                talleId: talleId
-            }
-        })
+    //Busco el stock updateado.
+    const updatedStock = await Producto_talle.findOne({
+      where: { productoId: productId, talleId: talleId },
+    });
 
-        //Busco el stock updateado
-        const updatedStock = await Producto_talle.findOne({where: {productoId: productId, talleId: talleId}})
+    //Si tengo un numero negativo en stock lo vuelvo a cambiar a 0.
+    if (updatedStock.dataValues.stock < 0)
+      await updatedStock.update({ stock: 0 });
 
-        //Mando el stock updateado
-        res.send(updatedStock)
-    }catch(e){
-        res.send(e)
-    }
-
-})
-
+    //Mando el stock updateado
+    return res.send(updatedStock);
+  } catch (e) {
+    return res.send(e);
+  }
+});
 
 module.exports = router;
