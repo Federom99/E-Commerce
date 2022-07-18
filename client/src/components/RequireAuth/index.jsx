3773d786523e9ced8ca002f4dd3b2755855eb1c0
Jose Osorio <joseinvictus273@hotmail.com>
2022-07-17 23:27:17 -0500

    Fix: Arreglado pequeño error cuando se usa cuenta admin

--- client/src/components/RequireAuth/index.jsx
+++ client/src/components/RequireAuth/index.jsx
@@ -5,7 +5,7 @@ function RequireAuth({isAdmin}){
     const auth = useSelector(state => state.auth)
     const location = useLocation();
     return (
-        auth?.user?.isAdmin === isAdmin
+        auth?.user?.isAdmin === isAdmin || auth?.isLoggedIn
         ? <Outlet/>
         : auth?.isLoggedIn
             ? <Navigate to='/' state={{from: location}} replace/>
