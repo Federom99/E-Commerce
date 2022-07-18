140e6138f331c92a66dce9130ec182a521be5ca7
AlanAcevedo12 <alanacevedo2001@hotmail.com>
2022-07-18 01:32:21 -0300

    Nuevo estilo al carrito

--- client/src/components/Cart/OrderItem/index.jsx
+++ client/src/components/Cart/OrderItem/index.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { BsTrashFill } from "react-icons/bs";
 import {
   addOrder,
   removeOrder,
   setLocalStorage,
 } from "../../../redux/actions/cart";
-import { List, Img, Li , Text , Amount, Button , Div , CloseButton} from "./styles";
+import { List, Img, Li , Text , Amount, Button , Div , CloseButton, PCant, SPAN, H3} from "./styles";
 
 export default function OrderItem({ id , item }) {
   const [productOrder,setOrder] = useState({
@@ -76,33 +77,33 @@ export default function OrderItem({ id , item }) {
   };
   return (
     <Div key={id}>
-      <CloseButton onClick={removeItem}>X</CloseButton>
+      <CloseButton onClick={removeItem}><BsTrashFill/></CloseButton>
       <List>
         <Li key={`${id}img`}>
           <Img src={`${item.imagen}`} alt={`Imagen de ${item.nombre}`} />
         </Li>
         <Li key={`${id}text`}>
           <Text>
-            <h3>{item.nombre}</h3>
-            {item.talle !=='Sin talle' ? (<h4>Talle: {item.talle}</h4>) : null }
+            <H3>{item.nombre}</H3>
+            {item.talle !=='Sin talle' ? (<H3>Talle: {item.talle}</H3>) : null }
             <h5>{item.descripcion}</h5>
           </Text>
         </Li>
         <Li key={`${id}price`}>
-          <h3>${item.precio}</h3>
+          <h3>${Intl.NumberFormat("es-AR").format(item.precio)}</h3>
         </Li>
         <Li key={`${id}amount`}>
           <Amount>
             <Button onClick={decAmount}>-</Button>
-            <p>{productOrder.cantidad}</p>
+            <PCant>{productOrder.cantidad}</PCant>
             <Button onClick={incAmount}>+</Button>            
-            {     
-              stock ? stock<=productOrder.cantidad ? (<span>Stock maximo</span>) : null : null
-            }
           </Amount>
+            {     
+              stock ? stock<=productOrder.cantidad ? (<SPAN>Stock maximo</SPAN>) : null : null
+            }
         </Li>
         <Li key={`${id}subtotal`}>
-          <h3>${productOrder.subtotal}</h3>
+          <h3>${Intl.NumberFormat("es-AR").format(productOrder.subtotal)}</h3>
         </Li>
       </List>
     </Div>
--- client/src/containers/CartContainer/index.jsx
+++ client/src/containers/CartContainer/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { List, Li , Error , Div , Header , CatList , Main, PriceSection} from "./styles";
+import { List, Li , Error , Div , Header , CatList , Main, PriceSection, BOTON} from "./styles";
 import OrderItem from '../../components/Cart/OrderItem';
 import { Link } from "react-router-dom";
 import { setLocalStorage } from "../../redux/actions/cart";
@@ -26,18 +26,17 @@ export default function ShoppingCart() {
   const price = order.reduce((prev,compra)=> prev+compra.subtotal,0)
   return (
     <Div>
- 
       { shoppingCart && shoppingCart.length ? (
         <List>
           <Li>
             <Header>
               {
-                  amount >1 ? <h1> Finalizar compra de {amount} productos </h1> : <h1> Finalizar compra </h1>
+                  amount >1 ? <p> Finalizar compra de {amount} productos </p> : <p> Finalizar compra </p>
               }
             </Header>
           </Li>
           <Li>
-            <section>
+            {/* <section>
               <CatList>
                 <Li><h2>Producto</h2></Li>
                 <Li><h2>Descripcion</h2></Li>
@@ -45,7 +44,7 @@ export default function ShoppingCart() {
                 <Li><h2>Cantidad</h2></Li>
                 <Li><h2>Subtotal</h2></Li>
               </CatList>
-            </section>
+            </section> */}
             <Main>
               {
               shoppingCart.map(item=><OrderItem key={`${item.id}+${item.talle}`} id={item.id} item={item}/>)
@@ -53,19 +52,23 @@ export default function ShoppingCart() {
             </Main>            
           </Li>
           <Li>
-            <PriceSection>Precio final: ${price}</PriceSection>
+            <PriceSection>Precio final: ${Intl.NumberFormat("es-AR").format(price)}</PriceSection>
           </Li>
-          {
-            currentUser ? (
-              <Link to="/checkout">
-                  <button>Iniciar compra</button>
-              </Link>
-            ) : (
-              <Link to="/login">
-                  <button>Inicia sesion para comprar</button>
-              </Link>
-            )
-          }
+          <Li>
+              <PriceSection>
+              {
+                currentUser ? (
+                  <Link to="/checkout">
+                      <BOTON>Iniciar compra</BOTON>
+                  </Link>
+                ) : (
+                  <Link to="/login">
+                      <BOTON>Inicia sesion para comprar</BOTON>
+                  </Link>
+                )
+              }
+              </PriceSection>
+          </Li>
         </List>
       ) : (
         <Error>No hay items en su carrito</Error>
