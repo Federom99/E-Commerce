1c72ba058b7fb1c24a93f50c3abb91ad6de9a5f2
tinchorlando <martin._orlando@hotmail.com.ar>
2022-07-17 22:01:02 -0300

    nueva animacion al entrar al popUp

--- client/src/components/Card/styles.js
+++ client/src/components/Card/styles.js
@@ -18,8 +18,44 @@ export const DIV = styled.div`
     }
 `
 export const StyledPopup = styled(Popup)`
+@keyframes anvil {
+  0% {
+    transform: scale(1) translateY(0px);
+    opacity: 0;
+    box-shadow: 0 0 0 rgba(241, 241, 241, 0);
+  }
+  1% {
+    transform: scale(0.96) translateY(10px);
+    opacity: 0;
+    box-shadow: 0 0 0 rgba(241, 241, 241, 0);
+  }
+  100% {
+    transform: scale(1) translateY(0px);
+    opacity: 1;
+    box-shadow: 0 0 500px rgba(241, 241, 241, 0);
+  }
+}
+@keyframes mymove {
+    0%{
+        background: rgba(0, 0, 0, 0);
+    }
+    25%{
+        background: rgba(0, 0, 0, 0.2);
+    }
+    50%{
+        background: rgba(0, 0, 0, 0.5);
+    }
+    100%{
+        background: rgba(0, 0, 0, 0.7);
+    }
+}
+
 &-overlay{
-    background: rgba(0, 0, 0, 0.7);
+    /* -webkit-animation: mymove 0.3s forwards; */
+    animation: mymove 0.3s forwards;
+}
+&-content {
+  animation: anvil 0.3s cubic-bezier(0.38, 0.1, 0.36, 0.9) forwards;
 }
 `
 
