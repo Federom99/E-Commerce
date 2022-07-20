f7867b3ad8014f6f4d00588847c9f92f73b1e236
Emiliano Arancio <93626345+Emiliar2002@users.noreply.github.com>
2022-07-20 05:05:39 -0300

    PUT /ban ahora banea

--- server/src/routes/ban.js
+++ server/src/routes/ban.js
@@ -1,9 +1,20 @@
 const { Router } = require("express");
+const {Usuario} = require('../db')
 
 const router = Router();
 
 router.put("/:id", async (req, res) => {
-    res.send(req.params.id)
+
+    const {id} = req.params
+
+    const user = await Usuario.findByPk(id)
+
+    await Usuario.update({
+        banned: !user.dataValues.banned
+    },
+    {where: {id: id}})
+
+    res.send(`Usuario ${id} baneado.`)
 });
 
 module.exports = router;
