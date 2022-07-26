645b20698c93baf28544ea6ef5f99af073c7d092
AlanAcevedo12 <alanacevedo2001@hotmail.com>
2022-07-26 15:33:16 -0300

    Se envian mails al terminar la compra

--- client/src/redux/actions/checkout.js
+++ client/src/redux/actions/checkout.js
@@ -121,7 +121,7 @@ export function updateUser(payload) {
 
 export function enviarMail(userMail) {
   return async function (dispatch) {
-    await axios.post(`${URL_SERVER}usuario/confirmacion`, { mail: userMail });
+    await axios.post(`${URL_SERVER}/usuario/confirmacion`, { mail: userMail });
 
     dispatch({ type: CONFIRMAR_COMPRA, payload: userMail });
   };
