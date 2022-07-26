6781c54cc3b6034709731c5b50e0150621da73c6
tinchorlando <92822629+tinchorlando@users.noreply.github.com>
2022-07-26 13:39:48 -0300

    Update styles.js

--- client/src/containers/CartContainer/styles.js
+++ client/src/containers/CartContainer/styles.js
@@ -84,8 +84,30 @@ right:0px;
 margin:10px;
 width: fit-content;
 `
-export const ToastList = styled.ul`
-    display:flex;
-    flex-direction:column;
-    list-style:none;    
+export const Button = styled.button`
+  margin: auto;
+  padding: 5px;
+  vertical-align: middle;
+  display: flex;
+  align-items: stretch;
+  height: auto;
+  background-color: #181818;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 1rem;
+  font-weight: bolder;
+  cursor: pointer;
+  &:hover {
+    background-color: whitesmoke;
+    color: black;
+    cursor: pointer;
+    border-color: black;
+    border-style: solid;
+  }
+`;
+
+export const Text = styled.p`
+  padding: 10px;
+  margin: 20px 0 0 0;
+  text-align: justify;
 `
