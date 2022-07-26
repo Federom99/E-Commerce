d3f7681943b6504b766311f60526f6350d56b595
Alexis <alexiscortazzii@gmail.com>
2022-07-26 15:05:17 -0300

    graficos

--- client/src/components/DashboardAdmin/graficoLineas.jsx
+++ client/src/components/DashboardAdmin/graficoLineas.jsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import { getPedidos } from '../../redux/actions/checkout';
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from 'react';
@@ -29,18 +28,34 @@ ChartJS.register(
 
 export default function LineChart() {
     const dispatch = useDispatch();
+    const [datos, setDatos] = useState([])
+    const [cambio, setCambiar] = useState(true)
 
     useEffect(() => {
         dispatch(getPedidos());
-    }, [])
+    }, [datos])
 
     const pedidos = useSelector((state) => state.checkout.pedidos);
-    console.log(pedidos)
-    console.log(pedidos.map(p => p.fecha.substring(0, 10)))
+    // console.log(pedidos)
+    // console.log(pedidos.map(p => p.fecha.substring(0, 10)))
     // console.log(pedidos.map(e => e.))
 
-    const scores = [6, 5, 5, 5, 3, 4, 6, 4, 5, 7];
-    const labels = pedidos.map(p => p.fecha.substring(0, 10));
+    var scores = [6, 5, 5, 5, 3, 4];
+    // const labels = [2017, 2018, 2019, 2020, 2021, 2022]
+    
+    var labels = ["Ene", "Feb", "Mzo", "Abr", "May", "Jun", "Jul"]
+    // const labels = pedidos.map(p => p.fecha.substring(0, 10));
+
+    function cambiar () {
+        setCambiar(!cambio)
+        // alert("Grafico")
+        console.log(labels)
+    }
+
+    cambio ? labels = [2016, 2017, 2018, 2019, 2020, 2021, 2022] : labels = ["Ene", "Feb", "Mzo", "Abr", "May", "Jun", "Jul"]
+    cambio ? scores = [12, 10, 10, 10, 6, 8, 21] : scores = [1, 2, 3, 2, 2, 4, 7]
+    console.log(cambio)
+    
 
     const options = {
         fill: true,
@@ -57,25 +72,41 @@ export default function LineChart() {
         },
     };
 
-    const data = useMemo(function () {
-        return {
-            datasets: [
-                {
-                    label: "Total Ventas",
-                    data: scores,
-                    tension: 0.3,
-                    borderColor: "rgb(75, 192, 192)",
-                    pointRadius: 6,
-                    pointBackgroundColor: "rgb(75, 192, 192)",
-                    backgroundColor: "rgba(75, 192, 192, 0.3)",
-                },
-            ],
-            labels,
-        };
-    }, []);
+    const data = {
+        datasets: [
+            {
+                label: cambio ? "Total Ventas por Año" : "Total ventas por Mes",
+                data: scores,
+                tension: 0.3,
+                borderColor: "rgb(75, 192, 192)",
+                pointRadius: 6,
+                pointBackgroundColor: "rgb(75, 192, 192)",
+                backgroundColor: "rgba(75, 192, 192, 0.3)",
+            },
+        ],
+        labels,
+    }
+
+    // const data = useMemo(function () {
+    //     return {
+    //         datasets: [
+    //             {
+    //                 label: "Total Ventas",
+    //                 data: scores,
+    //                 tension: 0.3,
+    //                 borderColor: "rgb(75, 192, 192)",
+    //                 pointRadius: 6,
+    //                 pointBackgroundColor: "rgb(75, 192, 192)",
+    //                 backgroundColor: "rgba(75, 192, 192, 0.3)",
+    //             },
+    //         ],
+    //         labels,
+    //     };
+    // }, []);
 
     return (
     <div className="grafico">
+        <button onClick={() => cambiar()}>Cambiar</button>
         <Line data={data} options={options} />
     </div>
     )
--- server/src/routes/cambiarEstadoPedido.js
+++ server/src/routes/cambiarEstadoPedido.js
@@ -4,30 +4,24 @@ const { Pedido } = require("../db.js");
 const router = Router();
 
 // PUT Pedido para poder cambiar el estado
-router.put("/:id", async (req, res) => {
+router.put("/", async (req, res) => {
+  const { id,
+    pago_total,
+    fecha,
+    direccion_de_envio,
+    estado } =
+    req.body;
+
   try {
-    const id = req.params.id;
-    const pedido = await Pedido.findByPk(id);
-    const { pago_total, direccion_de_envio, estado, fecha } = req.body;
-
-    if (pago_total) {
-      pedido.pago_total = pago_total;
-      pedido.save();
-    }
-
-    if (fecha) {
-      pedido.fecha = fecha;
-      pedido.save();
-    }
-
-    if (direccion_de_envio) {
-      pedido.Direccion_de_envio = direccion_de_envio;
-      pedido.save();
-    }
-    if (estado) {
-      pedido.Estado = estado;
-      pedido.save();
-    }
+    await Pedido.update(
+      {
+        pago_total,
+        fecha,
+        direccion_de_envio,
+        estado
+      },
+      { where: { id } }
+    );
     res.status(200).send(id);
   } catch (error) {
     res.status(400).send(error);
@@ -35,3 +29,35 @@ router.put("/:id", async (req, res) => {
 });
 
 module.exports = router;
+
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     const pedido = await Pedido.findByPk(id);
+//     const { pago_total, direccion_de_envio, estado, fecha } = req.body;
+
+//     if (pago_total) {
+//       pedido.pago_total = pago_total;
+//       pedido.save();
+//     }
+
+//     if (fecha) {
+//       pedido.fecha = fecha;
+//       pedido.save();
+//     }
+
+//     if (direccion_de_envio) {
+//       pedido.direccion_de_envio = direccion_de_envio;
+//       pedido.save();
+//     }
+//     if (estado) {
+//       pedido.estado = estado;
+//       pedido.save();
+//     }
+//     res.status(200).send(id);
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
+
+// module.exports = router;
