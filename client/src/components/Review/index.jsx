c6a659d02fe2980117de08b94ca194f88030fbdb
Davigetz <Davidstangetz13@gmail.com>
2022-07-27 00:50:10 -0500

    review terminado

--- client/src/components/Review/index.jsx
+++ client/src/components/Review/index.jsx
@@ -10,6 +10,7 @@ import { dateFormat } from "./dateformat";
 import {
   CloseBtn,
   Div,
+  Estado,
   H3,
   Img,
   Li,
@@ -29,12 +30,16 @@ function Review() {
   const change = useSelector((state) => state.reviews.modal);
 
   const close = () => dispatch(changeModalClose());
-  const open = (id, imagen, nombre, userId) =>
-    dispatch(changeModalOPen(id, imagen, nombre, userId));
-
+  const open = (id, imagen, nombre, userId, estado) => {
+    console.log(estado);
+    if (estado === "entregado") {
+      dispatch(changeModalOPen(id, imagen, nombre, userId, estado));
+    }
+  };
   useEffect(() => {
     dispatch(getPedidos(id));
   }, []);
+  // rojo a verde segun estado
 
   return (
     <Div key={0}>
@@ -43,6 +48,21 @@ function Review() {
       ) : (
         <div>
           {pedidos.map((pedido) => {
+            const colors = [
+              "#ff0088",
+              "#ff0000",
+              "#ff8800",
+              "#ffff00",
+              "#6aff00",
+              "#00ff22",
+            ];
+            let color;
+            if (pedido.estado === "Aprobado") color = colors[0];
+            if (pedido.estado === "en preparacion") color = colors[1];
+            else if (pedido.estado === "en camino") color = colors[2];
+            else if (pedido.estado === "en punto de entrega") color = colors[3];
+            else if (pedido.estado === "en poder del correo") color = colors[4];
+            else if (pedido.estado === "entregado") color = colors[5];
             return (
               <List key={pedido.id}>
                 <Li key={`${pedido.id}fecha`}>
@@ -64,7 +84,8 @@ function Review() {
                                 product.compra.productoId,
                                 product.imagen,
                                 product.nombre,
-                                id
+                                id,
+                                pedido.estado
                               )
                         }
                       >
@@ -86,6 +107,7 @@ function Review() {
                     $ {Intl.NumberFormat("es-AR").format(pedido.pago_total)}
                   </h4>
                 </Li>
+                <Estado color={color}>{pedido.estado}</Estado>
                 <Linea />
               </List>
             );
